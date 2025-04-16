669493a67402479fbb28e585995d1bb1055d8fa9
oobabooga <112222186+oobabooga@users.noreply.github.com>
2025-04-15 19:56:59 -0700

    Add CMakeExtension

--- setup.py
+++ setup.py
@@ -1,12 +1,54 @@
-from setuptools import setup, find_packages
+import glob
+import os
+import shutil
+import subprocess
+
+from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name):
+        # Don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+
+class CMakeBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_cmake(ext)
+
+    def build_cmake(self, ext):
+        # Get CMAKE_ARGS from environment
+        cmake_args = os.environ.get("CMAKE_ARGS", "")
+
+        # Create build directory
+        build_dir = os.path.abspath("build")
+        os.makedirs(build_dir, exist_ok=True)
+
+        # Get submodule path
+        llama_cpp_dir = os.path.abspath("llama.cpp")
+
+        # Configure with CMake
+        cmake_cmd = ["cmake", llama_cpp_dir] + cmake_args.split()
+        subprocess.check_call(cmake_cmd, cwd=build_dir)
+
+        # Build with multiple cores
+        build_cmd = ["cmake", "--build", ".", "--config", "Release", "-j"]
+        subprocess.check_call(build_cmd, cwd=build_dir)
+
+        # Copy all files from bin directory to package
+        bin_dir = os.path.join(build_dir, "bin")
+        target_dir = os.path.join(self.build_lib, "llama_cpp_binaries", "bin")
+        os.makedirs(target_dir, exist_ok=True)
+        for file in glob.glob(os.path.join(bin_dir, "*")):
+            shutil.copy(file, target_dir)
 
 setup(
     name="llama_cpp_binaries",
     version="0.1.0",
     description="Binaries for llama.cpp server",
     packages=find_packages(),
-    package_data={
-        'llama_cpp_binaries': ['bin/*'],
-    },
-    include_package_data=True,
+    ext_modules=[CMakeExtension("llama_cpp_binaries")],
+    cmdclass={"build_ext": CMakeBuild},
+    package_data={"llama_cpp_binaries": ["bin/*"]},
 )
